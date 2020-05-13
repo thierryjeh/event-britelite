fc733589b3ff53a8d129c73a69656f71f5ad8f82
thierryjeh <thierry.jeh@gmail.com>
2020-05-13 04:46:29 +0200

    litle change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 User.destroy_all
 Event.destroy_all
 Attendance.destroy_all
@@ -5,11 +7,11 @@ Attendance.destroy_all
 # User.create(first_name: 'jojo', last_name: 'toto', description: Faker::Hipster.sentence, email: 'jo.tournerie@gmail.com', encrypted_password: Faker::Internet.password(min_length: 8))
 
 20.times do |i|
-  User.create(first_name: Faker::TvShows::HeyArnold.character, last_name: , description: Faker::Hipster.sentence, email: 'example_' + i.to_s + '@caramail.com', password: Faker::Internet.password(min_length: 8))
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Hipster.sentence, email: 'example_' + i.to_s + '@yolo.com', password: Faker::Internet.password(min_length: 8))
 end
 
 5.times do
-  Event.create(start_date: Faker::Date.forward(days: 30), duration: Faker::Number.between(from: 1, to: 10) * 5, title: , description: Faker::TvShows::TwinPeaks.quote, price: Faker::Number.within(range: 1..1000), location: 'Aix-en-Provence')
+  Event.create(start_date: Faker::Date.forward(days: 30), duration: Faker::Number.between(from: 1, to: 10) * 5, title: Faker::Hipster.sentence(word_count: 3), description: Faker::Hipster.sentence(word_count: 6), price: Faker::Number.within(range: 1..1000), location: 'Bordeaux')
 end
 
 20.times do
